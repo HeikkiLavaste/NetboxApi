027ab4107061d5b18384b10ab0a545af55a7cc65
HeikkiLavaste <heikki.lavaste@hotmail.com>
2025-09-17 15:30:13 +0100

    changes

--- types.go
+++ types.go
@@ -1,8 +1,8 @@
 package netboxapi
 
 type Result struct {
-	Count    int                      `json:"count"`
-	Next     int                      `json:"next"`
-	Previous int                      `json:"previous"`
-	Results  []map[string]interface{} `json:"results"`
+	Count    int              `json:"count"`
+	Next     int              `json:"next"`
+	Previous int              `json:"previous"`
+	Results  []map[string]any `json:"results"`
 }
